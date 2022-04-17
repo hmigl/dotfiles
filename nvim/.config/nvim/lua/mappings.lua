d37011bfc5334be8173d8959bcb526fdbaaff625
hmigl <88746406+hmigl@users.noreply.github.com>
2022-04-17 16:35:52 -0300

    Refactor mappings file

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,59 +1,60 @@
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
-local keymap = vim.api.nvim_set_keymap
+local map = vim.api.nvim_set_keymap
 
 -- Modes
---   normal mode -> "n"
---   insert mode -> "i"
---   visual mode -> "v"
---   visual block mode -> "x"
+--  normal mode -> "n"
+--  insert mode -> "i"
+--  visual mode -> "v"
+--  visual block mode -> "x"
 
 -- Set leader key
 -- vim.g.mapleader = '\\' (backslash is used by default)
 
--- Navigation
-  -- NvimTree
-keymap('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
-  -- Splits
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
-  -- Buffers
-keymap('n', '<leader>h', ':bprevious<CR>', opts)
-keymap('n', '<leader>l', ':bnext<CR>', opts)
-  -- Tabs
-keymap('n', '<A-h>', ':tabprevious<CR>', opts)
-keymap('n', '<A-l>', ':tabnext<CR>', opts)
+-- Shortcutting...
+
+-- split navigation
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+-- buffer navigation
+map('n', '<leader>h', ':bprevious<CR>', opts)
+map('n', '<leader>l', ':bnext<CR>', opts)
+-- tab navigation
+map('n', '<A-h>', ':tabprevious<CR>', opts)
+map('n', '<A-l>', ':tabnext<CR>', opts)
 
 -- Resize splits
-keymap('n', '<C-Left>', ':vertical resize +2<CR>', opts)
-keymap('n', '<C-Right>', ':vertical resize -2<CR>', opts)
-keymap('n', '<C-Up>', ':resize +2<CR>', opts)
-keymap('n', '<C-Down>', ':resize -2<CR>', opts)
+map('n', '<C-Left>', ':vertical resize +2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize -2<CR>', opts)
+map('n', '<C-Up>', ':resize +2<CR>', opts)
+map('n', '<C-Down>', ':resize -2<CR>', opts)
 
 -- Saving/quitting
-keymap('n', '<C-s>', ':w<CR>', opts)
-keymap('n', '<C-q>', ':q<CR>', opts)
+map('n', '<C-s>', ':w<CR>', opts)
+map('n', '<C-q>', ':q<CR>', opts)
 
--- Indenting
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
+-- Move text...
 
--- Move text
-keymap('n', '<A-j>', ':m .+1<CR>==', opts)
-keymap('n', '<A-k>', ':m .-2<CR>==', opts)
--- Same, but in visual mode
-keymap('v', '<A-j>', ':m \'>+1<CR>==gv=gv', opts)
-keymap('v', '<A-k>', ':m \'<-2<CR>==gv=gv', opts)
-
--- Telescope
-keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
-keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+-- in normal mode
+map('n', '<A-j>', ':m .+1<CR>==', opts)
+map('n', '<A-k>', ':m .-2<CR>==', opts)
+-- in visual mode
+map('v', '<A-j>', ':m \'>+1<CR>==gv=gv', opts)
+map('v', '<A-k>', ':m \'<-2<CR>==gv=gv', opts)
 
 -- Built-in spell check
-keymap('n', '<leader>o', ':setlocal spell! spelllang=en_us<CR>', opts)
-keymap('n', '<leader>p', ':set spelllang=pt_br<CR>', opts)
+map('n', '<leader>o', ':setlocal spell! spelllang=en_us<CR>', opts)
+map('n', '<leader>p', ':set spelllang=pt_br<CR>', opts)
 
--- Shellcheck
-keymap('n', '<leader>s', ':!clear && shellcheck %<CR>', opts)
+-- Check file in shellcheck
+map('n', '<leader>s', ':!clear && shellcheck %<CR>', opts)
+
+-- Plugin mappings...
+
+-- Telescope
+map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
+map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+-- NvimTree
+map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
