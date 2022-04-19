d0eb06711abdb14d5f76cd79740911274fae6caf
hmigl <88746406+hmigl@users.noreply.github.com>
2022-04-19 07:32:08 -0300

    Simplify colorscheme file

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,18 +1,9 @@
--- vim.cmd "colorscheme default"
-
-vim.g.tokyonight_style = "night"
 local colorscheme = "gruvbox"
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found")
 	return
 end
 
--- use a colorscheme, but make background transparent:
-vim.cmd [[
-
-hi Normal guibg=NONE ctermbg=NONE
-
-]]
+-- Use a colorscheme, but make background transparent
+vim.cmd('hi Normal guibg=NONE')
