de1b2f5bb06ae38fa1229d11a48e2db131a1f885
hmigl <88746406+hmigl@users.noreply.github.com>
2022-04-30 11:12:46 -0300

    Add and remove some plugins; change style

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,88 +3,87 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-	  'git',
-	  'clone',
-	  '--depth',
-	  '1',
-	  'https://github.com/wbthomason/packer.nvim',
-	  install_path})
+   packer_bootstrap = fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path})
 end
 
 function get_setup(name)
-	return string.format('require("setup/%s")', name)
+   return string.format('require("setup/%s")', name)
 end
 
 return require('packer').startup(function(use)
-  -- My plugins here
-  use ("wbthomason/packer.nvim")
-  use ({ "kyazdani42/nvim-tree.lua", config = get_setup("tree") })
-  use ({
-	  "nvim-lualine/lualine.nvim", config = get_setup("lualine"),
-	  requires = { "kyazdani42/nvim-web-devicons", opt = true }
-  	})
+   -- My plugins here
 
-  -- colorschemes
-  use ("cocopon/iceberg.vim")
-  use ("folke/tokyonight.nvim")
-  use ("lunarvim/darkplus.nvim")
-  use ("morhetz/gruvbox")
+   -- Packer can manage itself
+   use 'wbthomason/packer.nvim'
 
-  -- nvim-cmp
-  use ({
-	  "hrsh7th/nvim-cmp", -- completion plugin
-	  requires = {
-		  { "hrsh7th/cmp-buffer" },
-		  { "hrsh7th/cmp-path" },
-		  { "hrsh7th/cmp-cmdline" },
-		  { "L3MON4D3/LuaSnip" }, -- snippet engine
-		  { "rafamadriz/friendly-snippets" }, -- a bunch of snippets
-		  { "saadparwaiz1/cmp_luasnip" },
-		  { "hrsh7th/cmp-nvim-lsp" }, -- provides lsp completions
-	  },
-	  config = get_setup("cmp"),
-  })
+   -- LSP
+   use 'neovim/nvim-lspconfig'
 
-  -- telescope
-  use ({
-	  "nvim-telescope/telescope.nvim", -- fuzzy finder
-	  requires = {
-		  { "nvim-lua/plenary.nvim" },
-		  { "nvim-lua/popup.nvim" },
-	  },
-	  --config = get_setup("telescope"),
-  })
+   -- Telescope
+   use {
+      'nvim-telescope/telescope.nvim', -- fuzzy finder
+       requires = {
+          { 'nvim-lua/plenary.nvim' },
+          { 'nvim-lua/popup.nvim' },
+       },
+    }
 
-  -- LSP
-  use ("neovim/nvim-lspconfig")
+   -- Treesitter
+   use {
+      'nvim-treesitter/nvim-treesitter',
+      config = get_setup("treesitter"),
+      run = ':TSUpdate',
+   }
+   use 'p00f/nvim-ts-rainbow'
 
-  -- Treesitter
-  use ({
-	  "nvim-treesitter/nvim-treesitter",
-	  config = get_setup("treesitter"),
-	  run = ":TSUpdate",
-  })
-  use ("p00f/nvim-ts-rainbow")
-  use({
-      "windwp/nvim-autopairs",
+   -- Cmp
+   use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+         { 'hrsh7th/cmp-buffer' },
+         { 'hrsh7th/cmp-path' },
+         { 'hrsh7th/cmp-cmdline' },
+         { 'L3MON4D3/LuaSnip' },
+         { 'rafamadriz/friendly-snippets' },
+         { 'saadparwaiz1/cmp_luasnip' },
+         { 'hrsh7th/cmp-nvim-lsp' },
+      },
+      config = get_setup("cmp"),
+   }
+
+   use {
+      'windwp/nvim-autopairs',
       after = "nvim-cmp",
       config = get_setup("autopairs"),
-    })
+   }
 
-  use({
-    "numToStr/Comment.nvim",
-    config = function()
-        require('Comment').setup()
-    end
-})
+   use {
+      'nvim-lualine/lualine.nvim', config = get_setup("lualine"),
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+   }
 
-  use ({ "norcalli/nvim-colorizer.lua", config = get_setup("colorizer") })
+   use {
+      'norcalli/nvim-colorizer.lua',
+      config = get_setup("colorizer")
+   }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+   use 'tpope/vim-surround'
+   use 'tpope/vim-commentary'
+
+   -- Colorschemes
+   use 'lunarvim/darkplus.nvim'
+   use 'morhetz/gruvbox'
+
+   -- Automatically set up your configuration after cloning packer.nvim
+   -- Put this at the end after all plugins
+   if packer_bootstrap then
+      require('packer').sync()
+   end
 end)
 
