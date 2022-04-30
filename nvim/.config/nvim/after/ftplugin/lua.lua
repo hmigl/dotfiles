ca68d011e8683dc5ac7fb40826d153768abd4218
hmigl <88746406+hmigl@users.noreply.github.com>
2022-04-30 10:34:48 -0300

    Add ftplugin dir
    With this, I can obtain different settings for
    different types of files.

---
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,3 @@
+vim.opt.shiftwidth = 3            -- size of an indent
+vim.opt.tabstop = 3               -- number of spaces that a tab counts for
+vim.opt.expandtab = true          -- use spaces instead of tabs
