6f5134eb9c70cda15fd09d9517f74c5cd66cd3ba
hmigl <88746406+hmigl@users.noreply.github.com>
2022-05-04 21:16:12 -0300

    Define global options

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,24 +1,29 @@
 -- :help options
-local opt = vim.opt                   -- to set options
+local opt = vim.opt               -- to set options
 -- opt.syntax = "enable"
-opt.hidden = true                     -- required to keep multiple buffers open
-opt.number = true                     -- show line numbers
-opt.relativenumber = true             -- show line relative numbers
-opt.wrap = false                      -- long lines will be displayed as just one
-opt.linebreak = true                  -- stop words being broken on wrap
-opt.encoding = "utf-8"                -- set default encoding to UTF-8
-opt.mouse = "a"                       -- enable mouse
-opt.clipboard = "unnamedplus"         -- copy paste between vim and everything else
-opt.splitright = true                 -- put new windows right of current
-opt.splitbelow = true                 -- put new windows below current
-opt.shiftwidth = 4                    -- size of an indent
-opt.tabstop = 4
-opt.expandtab = false                 -- use tabs, not spaces
-opt.smartindent = true                -- insert indents automatically
-opt.autoindent = true
-opt.hlsearch = false                  -- disable hlsearch
-opt.inccommand = "nosplit"            -- show substitutions incrementally
-opt.ignorecase = true                 -- ignore case
-opt.smartcase = true                  -- when seaching try to be smart about cases
-opt.termguicolors = true              --
-opt.scrolloff = 5                     -- start scrolling with 5 lines
+opt.hidden = true                 -- required to keep multiple buffers open
+opt.number = true                 -- show line numbers
+opt.relativenumber = true         -- show line relative numbers
+opt.mouse = "a"                   -- enable mouse
+opt.clipboard = "unnamedplus"     -- copy paste between vim and everything else
+opt.splitright = true             -- put new windows right of current
+opt.splitbelow = true             -- put new windows below current
+opt.smartindent = true            -- insert indents automatically
+opt.autoindent = true             -- auto indent new lines
+opt.hlsearch = false              -- disable hlsearch
+opt.inccommand = "nosplit"        -- show substitutions incrementally
+opt.ignorecase = true             -- ignore case
+opt.smartcase = true              -- when seaching try to be smart about cases
+opt.termguicolors = true          -- use more colors
+opt.scrolloff = 5                 -- start scrolling with 5 lines
+opt.signcolumn = "yes"            -- draw an extra column
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
