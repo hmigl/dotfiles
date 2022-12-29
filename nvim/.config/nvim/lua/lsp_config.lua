261f729b7fe29491a9a82528fb5b3155fbdd8921
hmigl <88746406+hmigl@users.noreply.github.com>
2022-12-29 09:16:08 -0300

    feat: add diagnostic keymaps

--- nvim/.config/nvim/lua/lsp_config.lua
+++ nvim/.config/nvim/lua/lsp_config.lua
@@ -56,3 +56,9 @@ require('mason-lspconfig').setup_handlers({
     }
   end,
 })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', ',e', vim.diagnostic.open_float)
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
