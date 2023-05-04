a03b5d6d51f1f76f3b059815300d346bfa7f138c
hmigl <88746406+hmigl@users.noreply.github.com>
2023-05-04 07:21:46 -0300

    feat: format on save

--- nvim/.config/nvim/lua/setup/null_ls.lua
+++ nvim/.config/nvim/lua/setup/null_ls.lua
@@ -6,6 +6,8 @@ end
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
     -- C/C++
@@ -23,4 +25,17 @@ null_ls.setup({
     diagnostics.eslint,
     formatting.prettier,
   },
+
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
